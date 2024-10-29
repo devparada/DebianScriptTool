338c1bd798e1cf7a6de3b285db63bfd8518cb70b
Raúl <raul.parada040103@gmail.com>
2024-10-29 11:36:18 +0100

    Varias mejoras y optimizaciones

--- main.sh
+++ main.sh
@@ -9,26 +9,49 @@ then
         if groups $usuario | grep -q '\bsudo\b'
         then
             echo "Introduce la contraseña de tu usuario:"
-            exec sudo "$0" "$@" $(whoami)
+            exec sudo "$0" "$@"
         else 
             echo "Introduce la contraseña de root"
             exec su -c "$0" "$@"
         fi
         # Si la contraseña es invalida sale del programa
         exit 1
+else
+    echo "Este script es necesario que lo ejecutes como usuario"
 fi
 
-# Para que cuando reinicie el script se guarde al usuario que ejecute el script
-usuario=$1
-echo "Contraseña válida"
+# Función que modifica el bash
+function modificarBash() {
+    # Comprueba si el archivo existe
+    if [ -f $1 ]
+    then
+        cat $1 > $ruta/.bashrc-seguridad
+        echo export PATH=""/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"" >> $1
+    fi
+}
+
+# Para que cuando reinicie el script se guarde al usuario que ejecute el script con o sin sudo
+usuario=$(logname 2>/dev/null)
 echo === Debian Script Tool ===
 echo "Bienvienido, $usuario a este script para personalizar Debian a tu gusto"
 echo "A continuación le haremos unas preguntas:"
-echo "¿Agregar a este usuario al grupo sudo para usar sudo su? (S/N):"
+
+echo "¿Actualiza el sistema? (S/N):"
 read opcion1
 
-# Si la respuesta es S se ejecuta sino muestra el mensaje
 if [[ $opcion1 == S ]]
+then
+    echo "Actualizando el sistema"
+    apt update -y
+    apt upgrade -y
+fi
+
+echo
+echo "¿Agregar a este usuario al grupo sudo para usar sudo su? (S/N):"
+read opcion2
+
+# Si la respuesta es S se ejecuta sino muestra el mensaje
+if [[ $opcion2 == S ]]
 then
     echo "Agregando este usuario al grupo sudo..."
     usermod -aG sudo $usuario
@@ -36,32 +59,21 @@ else
     echo "No se agrego el usuario al grupo sudo"
 fi
 
-echo "¿Añadir al PATH las rutas para ejecutar los programas de /bin y usr/bin de tu usuario? (S/N)"
-read opcion2
-
-if [[ $opcion2 == S ]]
-then
-    ruta="/home/$usuario"
-    archivo1="$ruta/.bashrc"
-    if [ -f $archivo1 ]
-    then
-        cat $archivo1 > $ruta/.bashrc-seguridad
-        echo export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin" >> $archivo1
-        echo "Listo"
-    fi
-fi
-
-echo "¿Añadir al PATH las rutas para ejecutar los programas de /bin y usr/bin del root? (S/N)"
+echo
+echo "¿Añadir al PATH las rutas para ejecutar los programas de /bin y usr/bin de tu usuario y root? (S/N)"
 read opcion3
 
 if [[ $opcion3 == S ]]
 then
+    ruta="/home/$usuario"
+    archivoBashrc="/home/$usuario/.bashrc"
+
+    # Llama a la función y pasa la ruta como primer parámetro
+    modificarBash $archivoBashrc
+
     ruta="/root"
-    archivo2="/root/.bashrc"
-    if [ -f $archivo2 ]
-    then
-        cat $archivo2 > $ruta/.bashrc-seguridad
-        echo export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin" >> $archivo2
-        echo "Listo"
-    fi
+    archivoBashrc="/root/.bashrc"
+
+    modificarBash $archivoBashrc
+    echo "Añadido al PATH del $usuario y del root"
 fi
